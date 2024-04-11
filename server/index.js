35e561a27f7a52283b120bf0a44612ca5133ef82
robel tadesse <robeltadesse37@gmail.com>
2024-04-12 10:23:45 +1200

    server

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ import helmet from 'helmet';
 import cors from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
-import clientRoutes from 'routes/client.js';
-import generalRoutes from 'routes/general.js';
-import managementRoutes from 'routes/management.js';
-import salesRoutes from 'routes/sales.js';
+import clientRoutes from './routes/client.js';
+import generalRoutes from './routes/general.js';
+import managementRoutes from './routes/management.js';
+import salesRoutes from './routes/sales.js';
 
 /*CONFIGURATION*/
 
@@ -21,3 +21,14 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(cors());
+
+/*MONGOOOSE sETUP*/
+
+const PORT = process.env.PORT || 9000;
+mongoose
+.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {app.listen(PORT, () => console.log(`Server port: ${PORT}`));})
+.catch((error) => console.log(`${error} did not connect`));
