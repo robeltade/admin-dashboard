911b6799cbd742a4b0fad445a58ca34ad42946d3
robel tadesse <robeltadesse37@gmail.com>
2024-04-12 07:19:23 +1200

    initializing

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import helmet from 'helmet';
+import cors from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import clientRoutes from 'routes/client.js';
+import generalRoutes from 'routes/general.js';
+import managementRoutes from 'routes/management.js';
+import salesRoutes from 'routes/sales.js';
+
+/*CONFIGURATION*/
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy : "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(cors());
